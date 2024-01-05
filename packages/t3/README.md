23ffd9bef7c44e3cadd2791e56a709ed70c82df6
plume zhou <jmyumao@126.com>
2024-01-05 16:21:23 +0800

    feat: t3

---
+++ packages/t3/README.md
@@ -0,0 +1,11 @@
+# `t3`
+
+> TODO: description
+
+## Usage
+
+```
+const t3 = require('t3');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/t3/__tests__/t3.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const t3 = require('..');
+const assert = require('assert').strict;
+
+assert.strictEqual(t3(), 'Hello from t3');
+console.info('t3 tests passed');
